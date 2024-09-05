45680be52c2824339e0efc5d407f8befe267919e
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-09-05 09:38:00 +0100

    update tape, credit to danielcristofani

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 
 void main() {
     FILE*f=fopen("o.c","w"),*in=fopen("c.bf","r");char b[1024];
-    fprintf(f,"#include<stdio.h>\nvoid main(){char t[1024]={0},*p=t;");
+    fprintf(f, "#include<stdio.h>\nchar t[1<<24],*p=t;\nvoid main(){"); 
     while(fgets(b,sizeof(b),in)) for(char*c=b;*c;c++)
         fprintf(f,*c=='+'?"++*p;":*c=='-'?"--*p;":*c=='<'?"p--;":*c=='>'?"p++;":*c=='.'?"putchar(*p);":*c==','?"*p=getchar();":*c=='['?"while(*p){":*c==']'?"}":"");
     fprintf(f,"}");
